99986c887bea2c00e20237aac3d11579107fcd88
didikstl <125580027+didikstl@users.noreply.github.com>
2023-12-08 12:17:05 +0200

    new_1_1

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,3 +1,92 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.currency.models import Rate, ContactUs, Source
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'buy',
+        'sell',
+        'type',
+        'source',
+        'created',
+    )
+    list_filter = (
+        'type',
+        'created',
+        'source',
+    )
+    search_fields = (
+        'buy',
+        'sell',
+        'source',
+    )
+
+    readonly_fields = (  # для ограничения корректировки данных
+        'buy',
+        'sell',
+    )
+
+    def has_delete_permission(self, request, obj=None):  # Ограничения по удалению
+        return True
+
+    def has_add_permission(self, request):  # Ограничения по добавлению рейтов
+        return True
+
+    def has_change_permission(self, request, obj=None):  # Ограничения по изменению рейтов если добавить FALSE
+        return True
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email',
+        'subject',
+        'message',
+    )
+    list_filter = ('email',)
+    search_fields = (
+        'email',
+        'subject',
+        'message',
+    )
+
+    def has_delete_permission(self, request, obj=None):  # Ограничения по удалению
+        return False
+
+    def has_add_permission(self, request):  # Ограничения по добавлению рейтов
+        return False
+
+    def has_change_permission(self, request, obj=None):  # Ограничения по изменению рейтов если добавить FALSE
+        return False
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'source_url',
+        'source_name',
+        'created',
+
+    )
+    list_filter = (
+        'source_url',
+        'source_name',
+    )
+    search_fields = (
+        'source_url',
+        'source_name',
+    )
+
+    def has_delete_permission(self, request, obj=None):  # Ограничения по удалению
+        return True
+
+    def has_add_permission(self, request):  # Ограничения по добавлению рейтов
+        return True
+
+    def has_change_permission(self, request, obj=None):  # Ограничения по изменению рейтов если добавить FALSE
+        return True
