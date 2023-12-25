8d7fd0ddcd0a787324c536c80670d196430c190d
didikstl <125580027+didikstl@users.noreply.github.com>
2023-12-25 14:40:47 +0200

    new_1_2

--- account/views.py
+++ account/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, RedirectView
+from account.forms import UserSignUpForm
 
-# Create your views here.
+from django.urls import reverse_lazy
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+User = get_user_model()
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    template_name = 'registration/profile.html'
+    success_url = reverse_lazy('Index')
+    fields = (
+        'first_name',
+        'last_name',
+    )
+
+    def get_object(self, queryset=None):
+        qs = self.get_queryset()
+        return qs.get(id=self.request.user.id)
+
+
+class UserSignUpView(CreateView):
+    queryset = User.objects.all()
+    template_name = 'signup.html'
+    success_url = reverse_lazy('Index')
+    form_class = UserSignUpForm
+
+
+class UserActivateView(RedirectView):
+    pattern_name = 'login'
+
+    def get_redirect_url(self, *args, **kwargs):
+        username = kwargs.pop('username')
+
+        user = User.objects.filter(username=username).only('id').first()
+
+        if user is not None:
+
+            user.is_active = True
+            user.save(update_fields=('is_active'))
+
+        return super().get_redirect_url(*args, **kwargs)
--- config/urls.py
+++ config/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('auth/', include('django.contrib.auth.urls')),
-
-    path('auth/profile/', ProfileView.as_view(), name='profile'),
+    path('auth/profile/',  include('account.urls')),
 
     path('', include('app.currency.urls')),
-
     path('', IndexView.as_view(), name='Index'),
 
     # Сброс пароля
@@ -22,6 +20,27 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
-
+from django.contrib import admin
+# from django.urls import path, include
+# from app.currency.views import tets_templates, IndexView
+# from django.contrib.auth import views as auth_views
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#
+#     path('auth/', include('django.contrib.auth.urls')),
+#     path('auth/profile/', include('account.urls')),
+#
+#
+#     path('', include('app.currency.urls')),
+#     path('template/', tets_templates),
+#     path('', IndexView.as_view(), name='Index'),
+#
+#     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+#     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+#     path('reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+#     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+#
+# ]
 
 
